153691718291f469216cc343b90bb5904af11242
Pham The Hung <hung.pt@aris-vn.com>
2024-01-23 10:55:30 +0700

    revert structure

--- Calendar.razor.cs
+++ Calendar.razor.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimpleComponents.Calendar
+namespace SimpleComponents
 {
     public partial class Calendar<T>
     {
