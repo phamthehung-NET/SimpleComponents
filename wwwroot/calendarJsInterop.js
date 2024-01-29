4d8da17f59967bb4227b72d5198d7a6b78519469
Pham The Hung <hung.pt@aris-vn.com>
2024-01-29 14:29:08 +0700

    update v0.0.3

--- wwwroot/calendarJsInterop.js
+++ wwwroot/calendarJsInterop.js
@@ -8,11 +8,29 @@ export function showPrompt(message) {
 export function resizeCalendarEvents() {
     let heights = [];
 
+    removeDragEventListener();
+
+    registDragEventListener();
+
     for (let i = 1; i <= 6; i++) {
+        // get the highest order of entry row on the grid
         const maxOrder = Math.max.apply(null, $(`.calendar-event[data-row=${i}]`).map(function () {
             return $(this).attr("data-order");
         }).get());
 
+        for (let order = 1; order <= maxOrder; order++) {
+            if (order > 1) {
+                // get the previous event to get height and top css attribute
+                const calendarItem = $(`.calendar-event[data-row=${i}][data-order=${order}]`);
+                const prevCalendarItem = $(`.calendar-event[data-row=${i}][data-order=${order - 1}]`);
+                const height = prevCalendarItem.outerHeight();
+                //substring 'px'
+                const top = parseInt(prevCalendarItem.css("top").substring(0, prevCalendarItem.css("top").length - 2))
+                calendarItem.css("top", `${height + top + 10}px`)
+            }
+        }
+
+        // get the highest height in rows on the grid to apply to the rest rows
         const maxHeight = Math.max.apply(null, $(`.calendar-event[data-row=${i}][data-order=${maxOrder}]`).map(function () {
             let top = parseInt($(this).css("top").substring(0, $(this).css("top").length - 2))
             return $(this).outerHeight() + top;
@@ -26,4 +44,14 @@ export function resizeCalendarEvents() {
     }
 
     $("div.pth-days.d-flex").css("height", `${Math.max.apply(null, heights)}px`);
+}
+
+function removeDragEventListener() {
+    $("body").off("dragstart", ".pth-day")
+}
+
+function registDragEventListener() {
+    $(".pth-day").on("dragstart", function (e) {
+        e.originalEvent.dataTransfer.setDragImage(new Image(), 0, 0);
+    })
 }
